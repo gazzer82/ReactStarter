5a7c8bb6edd04333f38a8abd7056603dd8ee3146
gazzer82 <contact@garethjeanne.co.uk>
2016-07-29 15:19:20 +0100

    update all package versions

--- server.js
+++ server.js
@@ -6,37 +6,35 @@ var app = express();
 var isDevelopmentENV = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'dist');
 
-if(!isDevelopmentENV){
-  app.use(express.static(static_path))
-  .get('/', function (req, res){
-    res.sendFile('index.html',{
-      root: static_path
-    });
-  }).listen(process.env.PORT || 8080, function (err){
-    if (err) { console.log(err)};
-    console.log('Listening at localhost:8080');
+var port = process.env.PORT || 8080;
+
+app.use(express.static(static_path))
+.get('/', function (req, res){
+  res.sendFile('index.html',{
+    root: static_path
   });
-} else {
-  if(isDevelopmentENV) {
+}).listen(port, function (err){
+  if (err) { console.log(err)};
+  console.log(`Node listening at port ${port}`);
+});
+if(isDevelopmentENV) {
 
-    var WebpackDevServer = require('webpack-dev-server');
-    var config = require('./webpack.config');
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.config');
 
-    new WebpackDevServer(webpack(config), {
-      publicPath: config.output.publicPath,
-      contentBase: static_path,
-      hot: true,
-      historyApiFallback: true,
-      devtool: 'eval-source-map',
-      output: {
-        pathinfo: true
-      }
-    }).listen(3000, 'localhost', function (err, result) {
-      if (err) {
-        console.log(err);
-      }
-
-      console.log('Listening at localhost:3000');
-    });
-  }
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    contentBase: static_path,
+    hot: true,
+    historyApiFallback: true,
+    devtool: 'eval-source-map',
+    output: {
+      pathinfo: true
+    }
+  }).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Webpack Dev Server listening at port 3000');
+  });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,6 @@ module.exports = {
     new webpack.NoErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: 'underscore-template-loader!./html/index_template.html',
-      //favicon: './images/favicon.ico',
       title: 'React Starter',
       inject: 'body',
       filename: '../index.html'
